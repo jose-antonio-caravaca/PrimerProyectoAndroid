ea037da1ec74ae9292097515b23a41c68c051df6
jose-antonio-caravaca <jose.antonio.caravaca>
2018-09-23 21:24:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jose/primerproyectoandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.jose.primerproyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button boton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boton=(Button)findViewById(R.id.button2);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mensaje="Bienvenido";
+                Toast.makeText(MainActivity.this,mensaje,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
